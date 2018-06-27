358d21f6c4a5164b6ca0768557cbb677e12d528d
Erika Balbino <erk.balbino@gmail.com>
2018-06-27 14:53:38 -0700

    Create Product model and seed the table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.delete_all
+
+1000.times do
+    Product.create(
+        title: Faker::Hacker.say_something_smart,
+        description: Faker::HarryPotter.quote,
+        price: rand(0 ... 9999)
+    ) 
+end
+
+products = Product.all
+
+puts Cowsay.say("Created #{products.count} products", :dragon)
