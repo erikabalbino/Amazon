b0d0e9bbcdb8a6d25264daa0ea79a34a9c06ab78
Erika Balbino <erk.balbino@gmail.com>
2018-06-27 16:58:00 -0700

    Update products column and create validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,36 @@
 class Product < ApplicationRecord
+
+    validates(
+        :title,
+        presence: true,
+        uniqueness: { case_sensitive: false }
+    )
+
+    validates(
+        :price,
+        numericality: {
+            greater_than: 0
+        }
+    )
+
+    validates(
+        :description,
+        presence: true,
+        length: {
+            minimum: 10
+        }
+    )
+
+    before_validation :set_default_price, :capitalize_product
+
+    private
+
+    def set_default_price
+        self.price ||= 1
+    end
+
+    def capitalize_product
+        self.title.capitalize!
+    end    
+
 end
