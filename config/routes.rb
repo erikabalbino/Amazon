836fd657d694bdb2197d6a043f78f09d825c86c5
Erika Balbino <erk.balbino@gmail.com>
2018-06-28 16:19:49 -0700

    Create CRUD for Product

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
 
+  #Routes to Prouct model
+  get "/products/new", to: "products#new", as: :new_product
+  post "/products", to: "products#create", as: :products
+  get "/products/:id", to: "products#show", as: :product
+  get "/products", to: "products#index"
+  delete "/products/:id", to: "products#destroy"
+  get "/products/:id/edit", to: "products#edit", as: :edit_product
+  patch "products/:id", to: "products#update"
+  put "products/:id", to: "products#update"
+
+
   get('/', {to: 'home#index', as: 'home'})
-
   get('/contact', {to: 'contact#index'})
-
   post('/contact_submit', {to: 'contact#create'})
-
   get('/about', {to: 'about#index'})
 
 end
